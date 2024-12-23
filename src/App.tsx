3191fae842e967912e8c617db9c0315434efd407
Kiran Raj <kiranrajbnadakambi@gmail.com>
2024-12-23 14:27:38 +0530

    Add and update task components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import React from "react";
-import TaskCard from "./TaskCard.jsx";
+import React from 'react';
+import TaskApp from "./TaskApp";
+
+import './App.css';
+
 
 function App() {
   return (
-    <div className="p-5">
-      <div className="mb-10">
-        <h1 className="text-2xl font-bold mb-4">Pending</h1>
-        <TaskCard
-          title="Build the websiclearte"
-          dueDate="10th April"
-          assigneeName="Rohit S"
-        />
-        <TaskCard
-          title="Add a blog"
-          dueDate="22nd March"
-          assigneeName="Rohit M"
-        />
-      </div>
-
-      <div>
-        <h1 className="text-2xl font-bold mb-4">Done</h1>
-        <TaskCard
-          title="Design a mock"
-          completedAtDate="10th April"
-          assigneeName="Rohit M"
-        />
-        <TaskCard
-          title="Get Appointment by principal"
-          completedAtDate="20th April"
-          assigneeName="Ajay S"
-        />
-      </div>
+    <div className="App">
+      <TaskApp />
     </div>
   );
 }
-
 export default App;
