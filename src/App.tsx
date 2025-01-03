d69dea46aaa313efe4bf5222537ec64c8a5aee63
Kiran Raj <kiranrajbnadakambi@gmail.com>
2025-01-03 19:23:48 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
 import TaskApp from "./TaskApp";
 import './App.css';
 import './TaskCard.css'; // If styles are added here
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />,
+  },
+]);
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 export default App;
